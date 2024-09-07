c1d5758aa760015b83577069072fa94c80bb635f
Alissa Huskey <alissa.huskey@gmail.com>
2024-09-07 15:58:38 -0600

    Add web references.

--- words/cli/lookup_cli.py
+++ words/cli/lookup_cli.py
@@ -13,6 +13,30 @@ from words import bp, WordsError  # noqa
 from words.web_references import REFERENCES
 
 
+def validate_reference(ctx, param, value):
+    """Validate web reference."""
+    if value and value not in REFERENCES.keys():
+        raise click.BadParameter("Invalid web reference. Try --list.")
+    return value
+
+
+def list_references():
+    """Return a table listing all references."""
+    table = Table(box=box.SIMPLE, show_header=False)
+    for ref in REFERENCES._items:
+        keys = always_iterable(getattr(ref, REFERENCES.key))
+        table.add_row(*keys, ref.name)
+    return table
+
+
+def list_resources(ref):
+    """Return a table listing all resources for a given reference."""
+    table = Table(box=box.SIMPLE, show_header=False, title=f"{ref.name} Resources")
+    for r in ref.resources._items:
+        table.add_row(r.option, r.name)
+    return table
+
+
 @click.command("lookup")
 @click.option(
     "-l", "--list",
@@ -20,48 +44,43 @@ from words.web_references import REFERENCES
     help="List web reference or resources of a reference with --ref.",
 )
 @click.option(
-    "-w", "--ref",
+    "-w", "--ref", "reference",
+    callback=validate_reference,
     help="Web reference to search.",
 )
 @click.option(
     "-r", "--res", "resource",
     help="Resource to search.",
 )
-@click.argument("word")
+@click.argument("word", required=False)
 def lookup_cmd(**kwargs):
     """Look up a word using a web resource."""
-
-    name = kwargs.get("ref")
+    name = kwargs.get("reference")
     ref = REFERENCES.get(name)
 
     if kwargs.get("list"):
-
-        table = Table(box=box.SIMPLE, show_header=False)
-
-        # list the reference resources
         if ref:
-            table.title = f"{ref.name} Resources"
-            for r in ref.resources._items:
-                table.add_row(r.option, r.name)
-                ...
+            table = list_resources(ref)
 
-        # list references
         else:
-            for ref in REFERENCES._items:
-                keys = always_iterable(getattr(ref, REFERENCES.key))
-                table.add_row(", ".join(keys), ref.name)
+            table = list_references()
 
         rprint(table)
         return
 
+    word = kwargs.get("word")
+    if not word:
+        raise click.BadParameter("Missing argument: 'WORD'.")
+
+    if not ref:
+        raise click.BadParameter("Web reference (-w/--ref) required to look up a word.")
+
     s = kwargs.get("resource")
+
     if not s:
-        raise Exception("You must pass a resource.")
-    word = kwargs.get("wordesource")
-    if not s:
-        raise Exception("You must pass a word.")
+        raise click.BadParameter("Missing option: -r/--resource.")
 
     resource = ref.resources.get(s)
-    url = resource.url(kwargs.get("word"))
+    url = resource.url(word)
     print(url)
     run(["open", url])
--- words/web_references.py
+++ words/web_references.py
@@ -31,4 +31,76 @@ REFERENCES = Collection(
             #  ),
         )
     ),
+    WebReference(
+        name="Wordnik",
+        options=("wn", "wordnik"),
+        resources=(
+            Resource(name="lookup", option="l", template="https://wordnik.com/words/${WORD}"),
+        )
+    ),
+    WebReference(
+        name="OneLook",
+        options=("ol", "onelook"),
+        help="? any letter, * any number of letters, # consonant, @ vowel, -abcd disallow letters, +abcd restrict to letters, //abcd// unscramble, expand:, reverse acronym pattern:meaning",
+        resources=(
+            Resource(name="Query", option="q", template="https://onelook.com/?w=${WORD}"),
+            Resource(name="Related Words", option="rel", template="https://onelook.com/?w=${WORD}&related=1"),
+            Resource(name="Mentions", option="men", template="https://onelook.com/?w=${WORD}&mentions=1"),
+            Resource(name="Lyrics", option="lyr", template="https://onelook.com/?w=${WORD}&verses=1"),
+            Resource(name="History", option="his", template="https://onelook.com/?w=${WORD}&history=1"),
+            Resource(name="Thesaurus", option="thes", template="https://onelook.com/thesaurus/?s=${WORD}"),
+        )
+    ),
+    WebReference(
+        name="Merriam-Webster",
+        options=("mw", "webster"),
+        resources=(
+            Resource(name="Dictionary", option="dict", template="https://www.merriam-webster.com/dictionary/${WORD}"),
+            Resource(name="Thesaurus", option="thes", template="https://www.merriam-webster.com/thesaurus/${WORD}"),
+        )
+    ),
+    WebReference(
+        name="Wiktionary",
+        options=("w", "wik"),
+        resources=(
+            Resource(name="Wiki", option="wiki", template="https://en.wiktionary.org/wiki/${WORD}"),
+        )
+    ),
+    WebReference(
+        name="Wikipedia",
+        options=("wp", "wikipedia"),
+        resources=(
+            Resource(name="Wiki", option="wiki", template="https://en.wikipedia.org/wiki/${WORD}"),
+        )
+    ),
+    WebReference(
+        name="Power Thesaurus",
+        options=("pt", "power"),
+        resources=(
+            Resource(name="Synonyms", option="syn", template="https://www.powerthesaurus.org/${WORD}/synonyms"),
+            Resource(name="Antonyms", option="ant", template="https://www.powerthesaurus.org/${WORD}/antonyms"),
+            Resource(name="Definitions", option="def", template="https://www.powerthesaurus.org/${WORD}/definitions"),
+            Resource(name="Sentences", option="sent", template="https://www.powerthesaurus.org/${WORD}/sentences"),
+            Resource(name="Thesaurus", option="thes", template="https://www.powerthesaurus.org/${WORD}"),
+        )
+    ),
+    WebReference(
+        name="Rhyme Zone",
+        options=("rz", "rhyme"),
+        resources=(
+            Resource(name="Rhymes", option="rhy", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&typeofrhyme=perfect&org1=syl&org2=l&org3=y"),
+            Resource(name="Near Rhymes", option="near", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=nry"),
+            Resource(name="Thesaurus", option="thes", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=syn"),
+            Resource(name="Phrases", option="phrase", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=phr"),
+            Resource(name="Phrase Rhymes", option="ph-rhy", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=pry"),
+            Resource(name="Descriptive Words", option="desc", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=jjb"),
+            Resource(name="Definitions", option="def", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=def"),
+            Resource(name="Similar Sounds", option="sound", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=sim"),
+            Resource(name="Same Consonants", option="cons", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&org1=syl&org2=l&org3=y&typeofrhyme=cons"),
+            Resource(name="Anagrams", option="ana", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&typeofrhyme=ana&org1=syl&org2=l&org3=y"),
+            Resource(name="Homophones", option="hom", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&typeofrhyme=hom&org1=syl&org2=l&org3=y"),
+            Resource(name="Similarly Spelled", option="spell", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&typeofrhyme=spell&org1=syl&org2=l&org3=y"),
+            Resource(name="Find", option="find", template="https://www.rhymezone.com/r/rhyme.cgi?Word=${WORD}&typeofrhyme=sub&org1=syl&org2=l&org3=y"),
+        )
+    ),
 )
